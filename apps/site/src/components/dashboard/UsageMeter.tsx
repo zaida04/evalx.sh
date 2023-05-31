2a80a21616be3b881727ec2a4011c902ceb87d3e
zaida04 <nico.03727+github@gmail.com>
2023-05-30 21:40:32 -0400

    fix: type error

--- apps/site/src/components/dashboard/UsageMeter.tsx
+++ apps/site/src/components/dashboard/UsageMeter.tsx
@@ -1,6 +1,6 @@
 export default function UsageMeter() {
   return <div className="flex flex-col items-center justify-center h-full">
     <h4 className="mb-4">Request Usage</h4>
-    <div className="radial-progress " style={{ "--value": 70, "--size": "6rem" }}>70%</div>
+    <div className="radial-progress " style={{ "--value": 70, "--size": "6rem" } as React.CSSProperties}>70%</div>
   </div>
 }
